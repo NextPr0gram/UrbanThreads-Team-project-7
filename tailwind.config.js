1eb66e10b598e607a3ed2588019f3ea4ba9f2614
NextProgram <anafkarim@proton.me>
2023-11-22 13:12:42 +0000

    created fonts folder in /public

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,19 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            fontSize: {
+                'xs': '0.75rem',    // Extra Small
+                'sm': '0.875rem',   // Small
+                'base': '0.875rem', // Base
+                'lg': '1.25rem',    // Large
+                'xl': '1.625rem',   // Extra Large
+                '2xl': '1.875rem',  // 2 Extra Large
+                '3xl': '1.875rem',  // 3 Extra Large
+                '4xl': '2.25rem',   // 4 Extra Large
+                '5xl': '3rem',      // 5 Extra Large
+                '6xl': '4rem',      // 6 Extra Large
+                // Add more custom sizes as needed
+              },
         },
     },
 
