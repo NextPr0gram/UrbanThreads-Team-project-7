ada58bff0b7c119838deed55f6635a1aa5bc5761
NextProgram <anafkarim@proton.me>
2023-11-22 13:02:04 +0000

    added account, shopping cart and wishlist icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,9 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+
+        {{-- original login and register links --}}
+        {{-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
@@ -29,7 +31,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif --}}
 
         </div>
     </body>
