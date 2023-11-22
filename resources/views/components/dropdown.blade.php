01b37377592c03278dc5834a3350d665c1d37775
NextProgram <anafkarim@proton.me>
2023-11-22 14:47:14 +0000

    turned the dropdown button in to an account button and added the account icon

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -21,13 +21,13 @@ switch ($width) {
 }
 @endphp
 
-<div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+<div class="relative " x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
     <div @click="open = ! open">
         {{ $trigger }}
     </div>
 
     <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
+            x-transition:enter="transition ease-out duration-200 "
             x-transition:enter-start="opacity-0 scale-95"
             x-transition:enter-end="opacity-100 scale-100"
             x-transition:leave="transition ease-in duration-75"
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,28 +16,35 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                        <button class="inline-flex items-center text-base  hover:underline">
+                            <div class="px-1">{{ Auth::user()->name }}</div>
                             <img src="{{asset('icons/account-icon-dark.svg')}}" alt="">
 
-                            <div class="ms-1">
+                            {{-- dropdown icon, uncomment to show--}}
+                            {{-- <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
-                            </div>
+                            </div> --}}
                         </button>
                     </x-slot>
+
+
+
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
+
+
 
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
@@ -51,6 +58,15 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+
+                {{-- Wishlist button --}}
+                <button class="inline-flex items-center text-base px-2 hover:underline ">
+                    <img src="{{asset('icons/wishlist-icon-dark.svg')}}" alt="">
+                </button>
+
+                {{-- Shopping cart button --}}
+                
+
             </div>
 
             <!-- Hamburger -->
