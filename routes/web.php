7d0140c8cc85dfbbe63db9c3413b90bd2cb9b80b
NextProgram <anafkarim@proton.me>
2023-11-22 12:27:02 +0000

    added route '/home'

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
