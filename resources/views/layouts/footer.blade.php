05517c16d65b4c8564cd428746dc5b76564c98fe
NextProgram <anafkarim@proton.me>
2023-11-27 20:44:51 +0000

    added appstore and playsotre images

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,95 +4,79 @@
     <div class="mx-auto w-full max-w-screen-xl">
         <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
           <div>
-              <h2 class="mb-6 text-sm font-semibold text-snow-white uppercase">Company</h2>
-              <ul class="text-snow-white font-medium">
+              <h2 class="mb-6 text-lg font-formula1 text-snow-white">Shop</h2>
+              <ul class="text-snow-white ">
                   <li class="mb-4">
-                      <a href="#" class=" hover:underline">About</a>
+                      <a href="#" class=" hover:underline">T-shirts</a>
                   </li>
                   <li class="mb-4">
-                      <a href="#" class="hover:underline">Careers</a>
+                      <a href="#" class="hover:underline">Hoodies</a>
                   </li>
                   <li class="mb-4">
-                      <a href="#" class="hover:underline">Brand Center</a>
+                      <a href="#" class="hover:underline">Trousers</a>
                   </li>
                   <li class="mb-4">
-                      <a href="#" class="hover:underline">Blog</a>
+                      <a href="#" class="hover:underline">Shirts</a>
                   </li>
               </ul>
           </div>
           <div>
-              <h2 class="mb-6 text-sm font-semibold text-snow-white uppercase">Help center</h2>
-              <ul class="text-snow-white font-medium">
+              <h2 class="mb-6 text-lg font-formula1 text-snow-white">Customer service</h2>
+              <ul class="text-snow-white ">
                   <li class="mb-4">
-                      <a href="#" class="hover:underline">Discord Server</a>
+                      <a href="#" class="hover:underline">Contact us</a>
                   </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Order status</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Shipping</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Give us feedback</a>
+                  </li>
+              </ul>
+          </div>
+          <div>
+              <h2 class="mb-6 text-lg font-formula1 text-snow-white">Social channels</h2>
+              <ul class="text-snow-white ">
                   <li class="mb-4">
                       <a href="#" class="hover:underline">Twitter</a>
                   </li>
                   <li class="mb-4">
-                      <a href="#" class="hover:underline">Facebook</a>
+                      <a href="#" class="hover:underline">Instagram</a>
                   </li>
                   <li class="mb-4">
-                      <a href="#" class="hover:underline">Contact Us</a>
+                      <a href="#" class="hover:underline">facebook</a>
                   </li>
+                  <li class="mb-4">
+                    <a href="#" class="hover:underline">LinkedIn</a>
+                </li>
               </ul>
           </div>
           <div>
-              <h2 class="mb-6 text-sm font-semibold text-snow-white uppercase">Legal</h2>
-              <ul class="text-snow-white font-medium">
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Privacy Policy</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Licensing</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                  </li>
-              </ul>
-          </div>
-          <div>
-              <h2 class="mb-6 text-sm font-semibold text-snow-white uppercase">Download</h2>
-              <ul class="text-snow-white font-medium">
+              <h2 class="mb-6 text-lg font-formula1 text-snow-white">Our Apps</h2>
+              <ul class="text-snow-white ">
                   <li class="mb-4">
                       <a href="#" class="hover:underline">iOS</a>
                   </li>
                   <li class="mb-4">
                       <a href="#" class="hover:underline">Android</a>
                   </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">Windows</a>
-                  </li>
-                  <li class="mb-4">
-                      <a href="#" class="hover:underline">MacOS</a>
-                  </li>
               </ul>
           </div>
       </div>
-
+    <hr class="my-6 border-black sm:mx-auto lg:my-8" />
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
-            </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-snow-white sm:mb-0">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
+        <div class=" sm:flex sm:items-center sm:justify-between">
+            <div class="flex justify-center items-center pb-4 sm:pb-0">
+                <x-application-logo class="block h-9 mx-2" />
+            </div>
+            <p class="flex flex-wrap justify-center mb-6 text-base text-snow-white sm:mb-0">
+                © UrbanThreads 2023 | All rights reserved
+            </p>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <span class="block text-sm text-snow-white sm:text-center">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
     </div>
 </footer>
+
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -145,7 +145,7 @@
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <div class="font-medium text-base text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
