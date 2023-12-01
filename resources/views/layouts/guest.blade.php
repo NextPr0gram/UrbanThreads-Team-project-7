68d81e9104f9099bbfad804a88c162728b939d65
NextProgram <anafkarim@proton.me>
2023-12-01 23:18:26 +0000

    added images for auth pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,8 @@
 <x-app-layout>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt">
+    <div class=" flex sm:justify-center items-center sm:pt">
+        <div class="">
+            <img src="images/auth/lady-image-auth.png" alt="">
+        </div>
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 b bg-white bg-opacity-40 shadow-md backdrop-blur-sm overflow-hidden border-2 border-navy-blue">
             <div class="shrink-0 flex items-center justify-center py-8">
                 <a href="{{ route('dashboard') }}">
@@ -7,6 +10,9 @@
                 </a>
             </div>
             {{ $slot }}
+        </div>
+        <div class="">
+            <img src="images/auth/man-image-auth.png" alt="">
         </div>
     </div>
 </x-app-layout>
