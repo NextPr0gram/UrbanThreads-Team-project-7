09eba6192466c0563e2f35e7dbfec9c009f9f555
NextProgram <anafkarim@proton.me>
2023-12-02 14:03:38 +0000

    images in auth pages now disappear when screen is smaller thatn lg

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="flex sm:justify-center items-center sm:pt py-32">
-        <div class="h-[40rem] -translate-x-[35rem] absolute">
+        <div class="hidden lg:block h-[40rem] -translate-x-[35rem] absolute">
             <img src="images/auth/lady-image-auth.png" alt="">
         </div>
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 b bg-white bg-opacity-40 shadow-md backdrop-blur-sm overflow-hidden border-2 border-navy-blue">
@@ -11,7 +11,7 @@
             </div>
             {{ $slot }}
         </div>
-        <div class="h-[40rem] absolute translate-x-[35rem]">
+        <div class="hidden lg:block h-[40rem] absolute translate-x-[35rem]">
             <img src="images/auth/man-image-auth.png" alt="">
         </div>
     </div>
