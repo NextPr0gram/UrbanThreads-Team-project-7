02e5384258c6669e16ffda98131283fa55863d99
NextProgram <anafkarim@proton.me>
2023-12-08 21:37:17 +0000

    removed old code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,6 @@
 <x-app-layout>
-    <!-- Container for "Welcome to UrbanThreads" -->
-    {{-- <div class="ml-16 sm:ml-6 mt-16 sm:pl-1 justify-center gap-y-2.5">
-
-    </div> --}}
-
-    {{-- <div class="flex mt-3 ml-16 text-black text-sm font-medium font-lexend leading-normal relative"> --}}
-        <!-- Heading 2 "Elevate Your Style. Discover the latest trends in fashion" -->
-
-        {{-- <div class="float-left w-2/3"> --}}
-            <!-- Pruple line to the left of the text (heading 2) -->
-            {{-- <span class="absolute left-0 inline-block px-0.5 h-10 sm:h-8 bg-bluish-purple ml-2 sm:ml-14"></span> --}}
-            {{-- <h2 class="">
-                Elevate Your Style. Discover the latest trends in fashion.
-            </h2>
-        </div>
-    </div> --}}
-
-    <!-- Container for "Shop now" button component -->
-    {{-- <div class="mt-3 sm:mt-4 ml-16 sm:ml-28 pl-2">
-        <a href="{{ route('products') }}">
-            <x-primary-button-dark> Shop now </x-primary-button-dark>
-        </a>
-    </div> --}}
-
     {{-- hero section --}}
-    <div class="flex justify-between items-center">
+    <div class="flex justify-between items-center lg:px-10">
         {{-- hero text --}}
         <div class="pl-4 py-10">
             <h1 class="px-2 my-5 text-bluish-purple text-3xl font-formula1 leading-tight sm:text-4xl lg:text-6xl">
@@ -41,11 +17,6 @@
         </div>
     </div>
 
-    <!-- Container for the people representing UrbanThreads -->
-    {{-- <div class="hidden xl:block absolute top-28 ml-12 right-4" style="top: 100px">
-        <img src="images/home-page-hero-image.png" alt="Image of people representing UrbanThreads"
-            class="float-right w-2/3 h-2/3" />
-    </div> --}}
 
     <!-- Container for the product category cards -->
     <div class="flex flex-col sm:flex-row items-center justify-evenly mt-36 sm:mt-80 mb-12 sm:mb-24">
