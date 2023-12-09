37fdc64a7e9886740ce552a8f6ad3f583e7a8024
NextProgram <anafkarim@proton.me>
2023-12-09 22:00:38 +0000

    removed products layout since it is unused

--- resources/views/layouts/products.blade.php
+++
@@ -1,5 +0,0 @@
-<x-app-layout>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {{ $slot }}
-    </div>
-</x-app-layout>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,14 +1,14 @@
 {{-- products.blade.php --}}
 
 <x-app-layout>
-    <div class="">
-        <h1 class="mt-5 ml-20 font-formula1 text-6xl">{{ $category }}</h1> <!-- This is the category title fetched by the product controller -->
-        <div class="grid grid-cols-3 mt-5 place-items-center justify-center content-center gap-y-5">
-            @foreach ($products as $product) <!-- This is the products array fetched by the product controller for all products in the category -->
-                <div>
-                    <a href="{{ route('show', ['slug' => $product->slug]) }}"> <!-- This is the route to the page for each individual product -->
-                        <x-products-card title='{{ $product->name }}' price='£{{ $product->selling_price }}'></x-products-card></a>
-                        <!-- This is the products image component, which is a card with the product name and price -->
+    <div class="container mx-auto">
+        <h1 class="mt-5 ml-4 md:ml-20 font-formula1 text-6xl">{{ $category }}</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 mt-5">
+            @foreach ($products as $product)
+                <div class="mb-4">
+                    <a href="{{ route('show', ['slug' => $product->slug]) }}">
+                        <x-products-card title='{{ $product->name }}' price='£{{ $product->selling_price }}'></x-products-card>
+                    </a>
                 </div>
             @endforeach
         </div>
