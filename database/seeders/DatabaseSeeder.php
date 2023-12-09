078fa961823fd8ab592a10088c8270788feb50e1
kishan <kishanjethwa224@gmail.com>
2023-12-09 16:32:35 +0000

    Delete unnecessary seeders for products and created simpler seeders for the category tables and product table.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,14 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
+use Database\Seeders\CategorySeeder;
 
-class ProductsSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $this->call([CategorySeeder::class]);
+        $this->call([ProductSeeder::class]);
     }
 }
