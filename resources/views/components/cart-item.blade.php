0344e22def126a9109aed4baa6dce0e1e557a31d
NextProgram <anafkarim@proton.me>
2023-12-09 16:14:22 +0000

    created cart item component

---
+++ resources/views/components/cart-item.blade.php
@@ -0,0 +1,11 @@
+<div class="flex flex-col sm:flex-row border-b border-bluish-purple max-sm:items-center">
+    <img class="m-2 h-24 w-28 border-2 border-bluish-purple object-cover object-center"
+        src="https://ih1.redbubble.net/image.339900741.3490/ssrco,triblend_tee,mens,navy_triblend,front,square_product,x600-bg,f8f8f8.u4.jpg"
+        alt="" />
+    <div class="flex w-full flex-col px-4 py-4 content-center">
+        <h2 class="text-lg font-formula1">{{ $productName }}</h2>
+        <p class="mt-auto text-lg font-formula1">{{ $price }}</p>
+        <p class="text-gray-400 font-lexend">Size: {{ $size }}</p>
+        <p class="text-gray-400 font-lexend">Colour: {{ $colour }}</p>
+    </div>
+</div>
