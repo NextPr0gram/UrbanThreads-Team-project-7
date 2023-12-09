f9d779f42d88765bc0438b1a3ab2f11a51cc4455
NextProgram <anafkarim@proton.me>
2023-12-09 23:27:35 +0000

    redone product cards

--- resources/views/components/products-card.blade.php
+++ resources/views/components/products-card.blade.php
@@ -1,12 +1,33 @@
-<div class="flex flex-col items-center">
+{{-- <div class="flex flex-col items-center">
     <div
         class="inline-flex flex-col justify-center items-center gap-26 w-64 h-64 mx-4 border-x-3 border-t-3 border-navy-blue bg-snow-white bg-opacity-60">
         Image goes here
     </div>
     <div
-        class="inline-flex flex-col justify-center items-center gap-26 w-64 h-24 mx-4 border-3 border-navy-blue bg-snow-white bg-opacity-60">
-        <h1 class="text-black font-Lexend Deca leading-normal text-sm text-left mt-2">{{ $title }}</h1>
-        <h1 class="text-black font-Lexend Deca leading-normal text-sm text-left">{{ $price }}</h1>
-        <x-primary-button-dark class="w-40 h-10 my-2" href={{ $productLink }}>View Product</x-primary-button-dark>
+        class="bg-snow-white">
+        <h1 class="text-black font-lexend text-sm mt-2">{{ $title }}</h1>
+        <h1 class="text-black font-lexend text-sm">{{ $price }}</h1>
+
+        <div class="flex w-full items-center justify-end p-4">
+            <x-primary-button-dark class="" href={{ $productLink }}>Add to basket</x-primary-button-dark>
+        </div>
+    </div>
+</div> --}}
+<div class="border-3 border-light-gray w-fit hover:border-bluish-purple hover:outline hover:outline-4 hover:outline-light-gray transition-all duration-300 ease-in-out">
+    {{-- image --}}
+    <div class="w-64 aspect-square bg-snow-white">
+        {{-- <img src="" alt=""> --}}
+
+    </div>
+
+    {{-- description --}}
+    <div class="px-4 bg-white">
+        <h1 class="font-formula1 text-md">{{ $title }}</h1>
+        <p>{{ $price }}</p>
+    </div>
+
+    {{-- button --}}
+    <div class="flex justify-end p-4 bg-white">
+        <x-primary-button-dark class="" href={{ $productLink }}>Add to basket</x-primary-button-dark>
     </div>
 </div>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,16 +1,22 @@
 {{-- products.blade.php --}}
 
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="">
+            {{ $category }}
+        </h2>
+    </x-slot>
     <div class="container mx-auto">
-        <h1 class="mt-5 ml-4 md:ml-20 font-formula1 text-6xl">{{ $category }}</h1>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 mt-5">
-            @foreach ($products as $product)
-                <div class="mb-4">
-                    <a href="{{ route('show', ['slug' => $product->slug]) }}">
-                        <x-products-card title='{{ $product->name }}' price='£{{ $product->selling_price }}'></x-products-card>
-                    </a>
-                </div>
-            @endforeach
+        <div class="flex justify-center">
+            <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mt-5">
+                @foreach ($products as $product)
+                    <div class="mb-4">
+                        <a href="{{ route('show', ['slug' => $product->slug]) }}">
+                            <x-products-card title='{{ $product->name }}' price='£{{ $product->selling_price }}'></x-products-card>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </x-app-layout>
