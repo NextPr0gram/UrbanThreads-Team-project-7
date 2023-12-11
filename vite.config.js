950d348aed4fb43f6792359150d8468402c5798e
NextProgram <anafkarim@proton.me>
2023-12-11 12:48:04 +0000

    added path to vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'public/**/*',
+
             ],
             refresh: true,
         }),
