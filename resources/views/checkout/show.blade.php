65da1810aaac98a94c1c01a234b0983a96bd8db4
NextProgram <anafkarim@proton.me>
2023-12-11 01:29:07 +0000

    polished the checkout page

--- resources/views/checkout/show.blade.php
+++ resources/views/checkout/show.blade.php
@@ -34,18 +34,18 @@
                     @endforeach
                 </div>
 
-                <div class="bg-white bg-opacity-60 p-3 mt-5 border-navy-blue border-2">
+                <div class="bg-white bg-opacity-60 p-4 mt-5 border-navy-blue border-2 space-y-6">
                     <h1 class="text-lg font-bold">Delivery Information</h1>
-                    <div class="mt-2 flex flex-col sm:flex-row gap-5">
+                    <div class="mt-2 flex flex-col sm:flex-row items-center sm:space-x-6">
                         <div class="w-full">
                             <x-input-label for="first_name">First Name</x-input-label>
                             <x-text-input type="text" id="first_name" name="first_name" class="font-lexend text-sm shadow-sm w-full"
-                                placeholder="John" />
+                                placeholder="John"/>
                         </div>
-                        <div class="w-full">
+                        <div class="w-full pt-6 sm:pt-0">
                             <x-input-label for="last_name">Last Name</x-input-label>
                             <x-text-input type="text" id="last_name" name="last_name" class="text-sm shadow-sm w-full"
-                                placeholder="Doe" />
+                                placeholder="Doe"/>
                         </div>
                     </div>
                     <div class="relative mt-2">
@@ -78,26 +78,26 @@
                 </div>
 
                 {{-- Payment information form --}}
-                <div class="bg-white bg-opacity-60 p-3 mt-5 border-navy-blue border-2 text-bluish-purple">
+                <div class="bg-white bg-opacity-60 p-4 mt-5 border-navy-blue border-2 space-y-6">
                     <h1 class="text-lg font-bold">Payment Information</h1>
-                    <div class="relative mt-2 w-full">
+                    <div class="relative w-full">
                         <x-input-label for="card_number">Card Number</x-input-label>
                         <x-text-input type="text" id="card_number" name="card_number"
                             class="text-sm shadow-sm w-full" placeholder="1234 5678 9012 3456" />
                     </div>
-                    <div class="flex flex-col sm:flex-row gap-5 mt-2">
+                    <div class="flex flex-col sm:flex-row sm:space-x-6">
                         <div class="w-full">
                             <x-input-label for="expiry_date">Expiry Date</x-input-label>
                             <x-text-input type="text" id="expiry_date" name="expiry_date"
                                 class="text-sm shadow-sm w-full" placeholder="MM/YY" />
                         </div>
-                        <div class="w-full">
+                        <div class="w-full pt-6 sm:pt-0">
                             <x-input-label for="security_code">Security Code (CVC/CVV)</x-input-label>
                             <x-text-input type="text" id="security_code" name="security_code"
                                 class="text-sm shadow-sm w-full" placeholder="123" />
                         </div>
                     </div>
-                    <div class="relative mt-2">
+                    <div class="relative">
                         <x-input-label for="cardholder_name">Cardholder Name</x-input-label>
                         <x-text-input type="text" id="cardholder_name" name="cardholder_name"
                             class="text-sm shadow-sm w-full" placeholder="Mr John Doe" />
@@ -105,21 +105,22 @@
                 </div>
             </div>
 
-            <div class="max-sm:px-4">
-                <div class="mt-10 space-y-3 px-2 py-4 sm:px-6 bg-white bg-opacity-60 border-2 border-navy-blue">
+            <div class="">
+                <div class="mt-10 space-y-6 p-4 sm:px-6 bg-white border-2 border-navy-blue">
                     <h1 class="text-lg font-bold">Order Summary</h1>
                     <!-- Discount code input -->
                     <form class="grid gap-6 mt-5">
                         <div class="flex flex-col space-x-3 sm:flex-row">
                             <x-text-input type="text" name="discount-code" class="w-full text-sm shadow-sm"
                                 placeholder="Enter a discount code here" />
-                            <div class="max-sm:mt-2 flex justify-center"><x-primary-button-dark
+                            <div class="max-sm:mt-2 flex justify-center "><x-primary-button-dark
                                     type="submit">Apply</x-primary-button-dark></div>
                         </div>
                     </form>
+                    
 
                     <!-- Total -->
-                    <div class="mt-6 border-t border-b border-bluish-purple py-2">
+                    <div class=" mt-6 border-t border-b border-bluish-purple py-2">
                         <div class="flex items-center justify-between">
                             <p class="text-sm font-medium text-gray-900">Number of items</p>
                             <p class="font-semibold text-gray-900">3</p>
