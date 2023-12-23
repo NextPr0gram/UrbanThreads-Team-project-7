9a5d12d49345dd952d413d511304957477bfa981
Kishan Jethwa <kishanjethwa224@gmail.com>
2023-12-23 17:07:46 +0000

    fix:duplicate column in reviews table

--- database/migrations/2023_12_23_122609_create_reviews_table.php
+++ database/migrations/2023_12_23_122609_create_reviews_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-
-            $table->id();
             $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('user_id');
             $table->string('user_name');
