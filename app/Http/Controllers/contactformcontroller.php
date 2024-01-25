60cc5052d4bceea71065b71e651c6c755de0fbcf
kishan <kishanjethwa224@gmail.com>
2024-01-25 20:58:53 +0000

    Refactor controller and model class names
    Update controller and model class names to follow naming conventions

--- app/Http/Controllers/contactformcontroller.php
+++ app/Http/Controllers/contactformcontroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class contactformcontroller extends Controller
+class ContactFormController extends Controller
 {
     //
 }
--- app/Http/Controllers/contactformscontroller.php
+++ app/Http/Controllers/contactformscontroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class contactformscontroller extends Controller
+class ContactFormsController extends Controller
 {
     //
 }
