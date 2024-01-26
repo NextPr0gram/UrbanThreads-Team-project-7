331e193ba69d8bd6a3eee30562b4e05dac52bb8d
kishan <kishanjethwa224@gmail.com>
2024-01-26 21:23:18 +0000

    Update address information form in profile view

--- resources/views/profile/partials/address-information-form.blade.php
+++ resources/views/profile/partials/address-information-form.blade.php
@@ -1,41 +1,45 @@
 <section>
     <header>
+        <!-- Title of form -->
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Address Information') }}
         </h2>
 
+        <!-- Description of form -->
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Save your address here for placing orders quickly.') }}
         </p>
     </header>
 
+    <!-- Form -->
     <form method="post" action="{{ route('profile.address') }}" class="mt-6 space-y-6">
         @csrf
         @method('put')
-
+        <!-- Form fields -->
+        <!-- The old values are set to the current values of the user's address if they exist (optional function) -->
         <div>
             <x-input-label for="address_line_1" :value="__('Address Line 1')" />
-            <x-text-input id="address_line_1" name="address_line_1" type="text" class="block mt-1 w-full" :value="old('address_line_1', $user->address->address_line_1)" required autofocus autocomplete="address_line_1" />
+            <x-text-input id="address_line_1" name="address_line_1" type="text" class="block mt-1 w-full" :value="old('address_line_1', optional($user->address)->address_line_1)" required autofocus autocomplete="address_line_1" />
             <x-input-error class="mt-2" :messages="$errors->get('address_line_1')" />
         </div>
         <div>
             <x-input-label for="address_line_2" :value="__('Address Line 2')" />
-            <x-text-input id="address_line_2" name="address_line_2" type="text" class="block mt-1 w-full" :value="old('address_line_2', $user->address->address_line_2)" autofocus autocomplete="address_line_2" />
+            <x-text-input id="address_line_2" name="address_line_2" type="text" class="block mt-1 w-full" :value="old('address_line_2', optional($user->address)->address_line_2)" autofocus autocomplete="address_line_2" />
             <x-input-error class="mt-2" :messages="$errors->get('address_line_2')" />
         </div>
         <div>
             <x-input-label for="city" :value="__('City')" />
-            <x-text-input id="city" name="city" type="text" class="block mt-1 w-full" :value="old('city', $user->address->city)" required autofocus autocomplete="city" />
+            <x-text-input id="city" name="city" type="text" class="block mt-1 w-full" :value="old('city', optional($user->address)->city)" required autofocus autocomplete="city" />
             <x-input-error class="mt-2" :messages="$errors->get('city')" />
         </div>
         <div>
             <x-input-label for="county" :value="__('County')" />
-            <x-text-input id="county" name="county" type="text" class="block mt-1 w-full" :value="old('county', $user->address->county)" required autofocus autocomplete="county" />
+            <x-text-input id="county" name="county" type="text" class="block mt-1 w-full" :value="old('county', optional($user->address)->county)" required autofocus autocomplete="county" />
             <x-input-error class="mt-2" :messages="$errors->get('county')" />
         </div>
         <div>
             <x-input-label for="postcode" :value="__('Postcode')" />
-            <x-text-input id="postcode" name="postcode" type="text" class="block mt-1 w-full" :value="old('postcode', $user->address->postcode)" required autofocus autocomplete="postcode" />
+            <x-text-input id="postcode" name="postcode" type="text" class="block mt-1 w-full" :value="old('postcode', optional($user->address)->postcode)" required autofocus autocomplete="postcode" />
             <x-input-error class="mt-2" :messages="$errors->get('postcode')" />
         </div>
 
