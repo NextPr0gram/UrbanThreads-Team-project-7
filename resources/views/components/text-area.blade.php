671a22dd02784731c0aa100989685a380d3c0eac
KavishaMistry <220032759@aston.ac.uk>
2024-02-07 09:50:42 +0000

    I have fixed he heading for the page, fixed spacing, fixed overflow, added comments to code

--- resources/views/components/text-area.blade.php
+++ resources/views/components/text-area.blade.php
@@ -1,3 +1,4 @@
 @props(['disabled' => false])
 
 <textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border border-1 border-neutral-60 p-2 bg-transparent text-black h-11 focus:border-primary-300 focus:ring-primary-300 rounded-sm']) !!}></textarea>
+
