84173913051e54763118fb3e8e5189b3e1a6a5b4
Neha Kerung <nehakerung@gmail.com>
2024-02-13 21:09:02 +0000

    Wishlists and its items conroller started

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -6,12 +6,30 @@ use Illuminate\Http\Request;
 
 class WishlistController extends Controller
 {
-    //
-    public function index()
+    //To show wishlists to users
+        public function show()
     {
-        // Command to fetch and return wishlist items
-        // $wishlistItems = Wishlist::all();
-        // return view('wishlist.index', compact('wishlistItems'));
+        // Get the authenticated user
+        $user = auth()->user();
+        // Get the wishlist of the authenticated user
+        if ($user) {
+            $wishlists = Wishlists::where('user_id', $user->id)->first();
+
+
+            if ($wishlists) {
+                // Optionally get the basket items of the basket if the basket exists
+                $wishlistItems = optional($wishlists)->items;
+                return view('wishlists.show', compact('wishlistItems')); //* Pass the basket items to the view
+                } else {
+                    return redirect()->back()->with('error', 'You do not have any items in your wishlist');
+                    //! Redirect to the previous page and displays an error message that the user does not have any items in their basket
+                }
+        } else {
+            //?? Really should be on when users try to add onto wishlist so could delete in future...
+            //! Redirect to the wishlist page if the user is not authenticated
+            return redirect()->route('login')->with('error', 'Login to view your wishlist');
+        }
+        
     }
   
 
