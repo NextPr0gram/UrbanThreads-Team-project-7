baedf7d0b6a64f0565a4e27b5c5a148a08c022af
NextProgram <anafkarim@proton.me>
2024-02-17 20:16:17 +0000

    fix: sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,5 @@
 <div id="overlay" class="hidden md:hidden absolute w-full h-screen opacity-50 bg-default-black transition-all duration-150 ease-in-out"></div>
-<div id="sideBar" class="absolute md:static z-10 h-screen bg-default-white w-3/4 md:w-[232px] bg-white shadow-sm border-r border-r-neutral-30 px-4 py-4 -translate-x-full md:translate-x-0 transition-all duration-150 ease-in-out rounded-r-lg md:rounded-[0px]">
+<div id="sideBar" class="text-base absolute md:static z-10 h-screen bg-default-white w-3/4 md:w-[232px] shrink-0 bg-white shadow-sm border-r border-r-neutral-30 px-4 py-4 -translate-x-full md:translate-x-0 transition-all duration-150 ease-in-out rounded-r-lg md:rounded-[0px]">
     <div class="flex items-center justify-between md:justify-center py-4 px-2">
         <h6 class="text-lg  font-formula1">Views</h6>
         <img onclick="closeSideBar()" class="md:hidden hover:cursor-pointer" src="{{ asset('icons/admin-dashboard/cancel-icon.svg') }}" alt="" />
