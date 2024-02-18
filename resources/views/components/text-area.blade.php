6393daf2b7f7463f563006feffff8e4ea24b5e3f
NextProgram <anafkarim@proton.me>
2024-02-18 16:19:17 +0000

    style: fix text-input styling to follow the figma

--- resources/views/components/text-area.blade.php
+++ resources/views/components/text-area.blade.php
@@ -1,4 +1,4 @@
 @props(['disabled' => false])
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border border-1 border-neutral-60 p-2 bg-transparent text-black h-11 focus:border-primary-300 focus:ring-primary-300 rounded-sm']) !!}></textarea>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border border-1 border-neutral-60 p-2 bg-transparent text-black h-11 focus:border-primary-300 focus:ring-primary-300 rounded-sm placeholder:text-neutral-60 placeholder:text-base']) !!}></textarea>
 
