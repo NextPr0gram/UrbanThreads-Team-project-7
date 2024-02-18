24f6ada45d3ff09720368ca2b701c18c807167d1
NextProgram <anafkarim@proton.me>
2024-02-18 23:58:49 +0000

    feat: add totalStock to AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers\Admin;
 
-
 use Illuminate\Http\Request;
 use App\Models\Product;
 use app\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-
     public function getAllProducts()
     {
-        $products = $products = Product::all(); // get all the products from the database
-        return view('admin.products-view', ['products' => $products]); // return the view and pass the products as a parameter
+        $products = Product::with('variations')->get(); // Eager load the variations relationship
+        $products->each(function ($product) {
+            $product->totalStock = $product->variations->sum('stock'); // Calculate the total stock for each product
+        });
+        return view('admin.products-view', ['products' => $products,]); // Return the view and pass the products as a parameter
     }
 }
