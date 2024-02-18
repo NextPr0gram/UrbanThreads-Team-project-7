ff5c761de20480af448eabac72e6d593399e621b
NextProgram <anafkarim@proton.me>
2024-02-18 16:19:06 +0000

    style: fix text-input styling to follow the figma

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-neutral-900 dark:text-gray-300']) }}>
+<label {{ $attributes->merge(['class' => ' text-neutral-900 text-base ']) }}>
     {{ $value ?? $slot }}
 </label>
