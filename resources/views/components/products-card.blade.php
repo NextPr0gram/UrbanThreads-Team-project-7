50d11687cc975e5c9e3e8caf85aeddbcff7079a3
hanad <220149138@aston.ac.uk>
2024-02-20 11:06:28 +0000

    changes to wishlist page and created heart for products

--- resources/views/components/products-card.blade.php
+++ resources/views/components/products-card.blade.php
@@ -16,7 +16,7 @@
         <p>{{ $price }}</p>
     </div>
 
-    <div class="ml-3 pt-3">
+    <div class="p-3">
         <button x-data="{ clicked: false }" @click="clicked = !clicked">
             <img src="{{ asset('icons/utility/heart-hover.svg') }}" class="w-6 h-5" :class="{ 'hidden': clicked }" alt="">
             <img src="{{ asset('icons/utility/heart-default.svg') }}" class="w-6 h-5" x-show="clicked" alt="">
--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -12,24 +12,24 @@
         </h2>
     </x-slot>
 
-    {{-- Wishlist Sections --}}
+    {{-- Wishlist Items Container --}}
+    <div class="flex flex-col items-center md:flex-row md:justify-evenly">
 
-    <div class="flex justify-center items-center ">
-        <div class="transition-all duration-300 ease-in-out border-3 border-light-gray w-fit hover:border-bluish-purple hover:outline hover:outline-4 hover:outline-light-gray">
+        {{-- Item 1 Container --}}
+        <div class="transition-all duration-300 ease-in-out border-3 border-light-gray w-fit hover:border-bluish-purple hover:outline hover:outline-4 hover:outline-light-gray mb-24 mt-5">
             <div class="w-64 aspect-square">
                 <!-- {{--* The placeholder for the image of the product --}} -->
-
-                <img class="w-64 aspect-square" src="#" alt="">
+                <img class="w-64 aspect-square" src="{{ asset('images/product-images/hoodies/comfy-hoodie.png') }}" alt=""> <!-- w-64 aspect-square -->
             </div>
 
-            <div class="px-4 py-1 bg-white">
+            <div class="px-4 bg-white">
                 {{--* The placeholders for the product name, price and availability --}}
                 <p class="text-base font-formula1">Pink Hoodie</p>
                 <p>£50.00</p>
                 <p>In Stock</p>
             </div>
 
-            <div class="pl-4 pt-4">
+            <div class="pt-4 pl-4">
                 {{-- *Button to remove from the wishlist --}}
                 <button x-data="{ clicked: false }" @click="clicked = !clicked">
                     <img src="{{ asset('icons/utility/heart-default.svg') }}" class="w-6 h-5" :class="{ 'hidden': clicked }" alt="">
@@ -39,13 +39,41 @@
 
             <div class="flex justify-end p-4 bg-white">
                 {{--* Button to add the product to the basket --}}
-                <x-primary-button class="" href={{ $productLink }}>More info</x-primary-button>
+                <x-primary-button class="" href={{ $productLink }}>Add to cart</x-primary-button>
                 {{--? The $productLink variable is the placeholder for the link to the product page of the specific product --}}
             </div>
-
         </div>
-    </div>
 
+        {{-- Item 2 Container --}}
+        <div class="transition-all duration-300 ease-in-out border-3 border-light-gray w-fit hover:border-bluish-purple hover:outline hover:outline-4 hover:outline-light-gray mb-24 mt-5">
+            <div class="w-64 aspect-square">
+                <!-- {{--* The placeholder for the image of the product --}} -->
+                <img class="w-64 aspect-square" src="{{ asset('images/product-images/hoodies/comfy-hoodie.png') }}" alt=""> <!-- w-64 aspect-square -->
+            </div>
+
+            <div class="px-4 bg-white">
+                {{--* The placeholders for the product name, price and availability --}}
+                <p class="text-base font-formula1">Pink Hoodie</p>
+                <p>£50.00</p>
+                <p>In Stock</p>
+            </div>
+
+            <div class="pt-4 pl-4">
+                {{-- *Button to remove from the wishlist --}}
+                <button x-data="{ clicked: false }" @click="clicked = !clicked">
+                    <img src="{{ asset('icons/utility/heart-default.svg') }}" class="w-6 h-5" :class="{ 'hidden': clicked }" alt="">
+                    <img src="{{ asset('icons/utility/heart-hover.svg') }}" class="w-6 h-5" x-show="clicked" alt="">
+                </button>
+            </div>
+
+            <div class="flex justify-end p-4 bg-white">
+                {{--* Button to add the product to the basket --}}
+                <x-primary-button class="" href={{ $productLink }}>Add to cart</x-primary-button>
+                {{--? The $productLink variable is the placeholder for the link to the product page of the specific product --}}
+            </div>
+        </div>
+
+    </div>
 
 </x-app-layout>
 
