42a06b1d7bf199e7ee7a8e9ddd7f6471aa83d896
NextProgram <anafkarim@proton.me>
2024-03-13 07:30:45 +0000

    feat: add logout button in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,8 +2,8 @@
     class="hidden lg:hidden absolute w-full h-screen opacity-50 bg-default-black transition-all duration-150 ease-in-out">
 </div>
 <div id="sideBar"
-    class=" text-base absolute lg:static z-10 h-screen bg-secondary-500 w-3/4 lg:w-[232px] shrink-0 bg-white shadow-sm  px-4 py-4 -translate-x-full lg:translate-x-0 transition-all duration-150 ease-in-out rounded-r-lg lg:rounded-[0px]">
-    <div class="flex items-center justify-between lg:justify-center py-4 px-2">
+    class=" flex flex-col text-base absolute lg:static z-10 h-screen bg-secondary-500 w-3/4 lg:w-[232px] shrink-0 bg-white shadow-sm  px-4 py-4 -translate-x-full lg:translate-x-0 transition-all duration-150 ease-in-out rounded-r-lg lg:rounded-[0px]">
+    <div class="pb-8 flex items-center justify-between lg:justify-center py-4 px-2">
         <h6 class="text-lg text-neutral-30 font-formula1">Views</h6>
         <img onclick="closeSideBar()" class="lg:hidden hover:cursor-pointer"
             src="{{ asset('icons/admin-dashboard/cancel-icon.svg') }}" alt="" />
@@ -13,6 +13,13 @@
     <x-sidebar-item title="User Accounts" href="admin.user-accounts-view" />
     <x-sidebar-item title="Customer Enquiries" href="admin.customer-enquiries-view" />
     <x-sidebar-item title="Orders" href="admin.orders-view" />
+
+    {{-- lower part of the sidebar --}}
+    <div class="flex flex-col grow justify-end">
+        <x-sidebar-item forLogout />
+    </div>
+
+
 </div>
 
 <script>
