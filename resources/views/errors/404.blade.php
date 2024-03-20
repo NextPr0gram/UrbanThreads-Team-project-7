31f40538f87bbf2b6904c182e1fd172ae18052a5
KishanJ22 <kishanjethwa224@gmail.com>
2024-03-20 09:26:16 +0000

    Add link to homepage on 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,14 +1,17 @@
 <x-app-layout>
-    <div class="flex justify-center py-20" >
-        <div class="flex flex-col justify-center py-16 bg-white bg-opacity-40 backdrop-blur sm:flex-row w-fit border-3 border-navy-blue rounded-xg">
-            <div class="px-8" ><img src="\images\errors\sad-pepe-404.svg" alt=""></div>
+    <div class="flex justify-center py-20">
+        <div
+            class="flex flex-col justify-center py-16 bg-white bg-opacity-40 backdrop-blur sm:flex-row w-fit border-3 border-navy-blue rounded-xg">
+            <div class="px-8"><img src="\images\errors\sad-pepe-404.svg" alt=""></div>
             <div class="block self-center px-8">
-                <h1 class="text-6xl font-formula1 text-bluish-purple" >404</h1>
-                <h2 class="py-2 text-3xl font-bold" >UH OH! Your're lost.</h2>
-                <p class="py-2" >The page you are looking for does not exist. How you got here is a <br>
+                <h1 class="text-6xl font-formula1 text-bluish-purple">404</h1>
+                <h2 class="py-2 text-3xl font-bold">UH OH! Your're lost.</h2>
+                <p class="py-2">The page you are looking for does not exist. How you got here is a <br>
                     mystery. But you can click the button below to go back to the homepage
                 </p>
-                <div class="py-2" ><x-primary-button>Home</x-primary-button></div>
+                <div class="py-2">
+                    <a href="{{ route('home') }}"><x-primary-button>Home</x-primary-button></a>
+                </div>
             </div>
         </div>
     </div>
