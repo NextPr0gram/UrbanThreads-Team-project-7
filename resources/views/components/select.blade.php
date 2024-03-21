31e29103c28baf99f10a7e838e8c4ff8ccdcd4bb
kishan <kishanjethwa224@gmail.com>
2024-03-21 12:54:20 +0000

    Update select and text-input component styles Fix 'Add to Basket' button not working on wishlist item

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,5 +1,9 @@
 <!-- resources/views/components/select.blade.php -->
 
-<select {{ $attributes->merge(['class' => 'border rounded p-2']) }}>
+<select
+    {{ $attributes->merge([
+        'class' =>
+            'border border-1 border-neutral-60 text-base bg-transparent focus:ring-0 text-neutral-900 h-[2.375rem] sm:h-10 md:h-11 focus:border-primary-300 focus:border-2 placeholder:text-neutral-60 placeholder:text-base  rounded-sm',
+    ]) }}>
     {{ $slot }}
 </select>
--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -15,65 +15,70 @@
             <div class="grid grid-rows-1 mt-5 gap-10 md:w-[600px] lg:w-[800px] xl:w-[1000px] 2xl:w-[1200px]">
 
                 {{-- Wishlist Items Container --}}
-                @foreach($wishlistItems as $item)
-                <div class="border-2 border-gray-0 rounded-md">
+                @foreach ($wishlistItems as $item)
+                    <div class="border-2 border-gray-0 rounded-md">
 
-                    {{-- Flex Container for Wishlist Item --}}
-                    <div class="flex flex-col md:flex-row items-center justify-between p-4 gap-4">
+                        {{-- Flex Container for Wishlist Item --}}
+                        <div class="flex flex-col md:flex-row items-center justify-between p-4 gap-4">
 
-                        <div class="flex flex-col md:flex-row items-center w-full md:w-auto">
-                            {{-- Product Image --}}
-                            <div class="w-24 aspect-square">
-                                <img class="w-24 aspect-square" src="{{ asset($item->product->image) }}" alt="{{ $item->product->name }}" title="{{ $item->product->name }}">
-                            </div>
+                            <div class="flex flex-col md:flex-row items-center w-full md:w-auto">
+                                {{-- Product Image --}}
+                                <div class="w-24 aspect-square">
+                                    <img class="w-24 aspect-square rounded-md" src="{{ asset($item->product->image) }}"
+                                        alt="{{ $item->product->name }}" title="{{ $item->product->name }}">
+                                </div>
 
-                            {{-- Product Details --}}
-                            <div class="pt-2 md:px-4 bg-white items-center">
-                                <div class="text-center md:text-left">
-                                    <p class="text-base font-semibold">{{ $item->product->name }}</p>
-                                    <p>{{ $item->product->original_price }}</p>
+                                {{-- Product Details --}}
+                                <div class="pt-2 md:px-4 bg-white items-center">
+                                    <div class="text-center md:text-left">
+                                        <p class="text-base font-semibold">{{ $item->product->name }}</p>
+                                        <p>{{ $item->product->selling_price }}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        {{-- Size Selector --}}
-                        <div x-data="{ open: false, selected: '' }" @click.away="open = false" class="relative text-neutral-900">
-                            <button @click="open = !open" class="flex justify-between items-center w-full md:w-64 xl:w-80 2xl:w-80 text-left px-2 py-1 bg-white border-2 border-gray-0 rounded-md md:px-4 md:py-2">
-                                <span x-text="selected === '' ? 'Select size' : selected"></span>
-                                <img :src="open ? '{{ asset('icons/utility/dropdown-arrow-up.svg') }}' : '{{ asset('icons/utility/dropdown-arrow-down.svg') }}'" alt="Toggle Dropdown" class="w-4 h-4 ml-2">
-                            </button>
-                            <div x-show="open" class="absolute w-42 md:w-64 xl:w-80 rounded-md bg-white shadow-lg z-10">
-                                <ul class="py-1 text-sm border-2 border-gray-0 rounded-md">
-                                    @foreach ($item->product->variations as $variation)
-                                    <li @click="selected = '{{ $variation->size }}'; open = false" class="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                                        Size: {{ $variation->size }} @if ($variation->stock <= 0) - Out of stock @elseif ($variation->stock < 10) - Low stock ({{ $variation->stock }} left) @else - In stock @endif </li>
-                                                @endforeach
-                                </ul>
-                            </div>
-                        </div>
-
-
-                        {{-- Wishlist & Cart Action Buttons --}}
-                        <div class="flex flex-row items-center md:flex-col md:items-end">
-                            {{-- Remove from Wishlist Button --}}
-                            <form action="{{ route('wishlist.remove', $item->product->id) }}" method="post">
+                            {{-- Size Selector --}}
+                            <form id="addToBasket" class="w-full md:w-auto"
+                                action="{{ route('basket.add', ['productId' => $item->product->id]) }}" method="post">
                                 @csrf
-                                @method('delete')
-                                <button type="submit" class="px-4">
-                                    <img src="{{ asset('icons/utility/heart-default.svg') }}" class="w-6 h-5" alt="">
-                                </button>
+                                <x-select id="size" name="size" class="w-full" required>
+                                    @foreach ($item->product->variations as $variation)
+                                        <option value="{{ $variation->id }}">Size: {{ $variation->size }}
+                                            @if ($variation->stock <= 0)
+                                                - Out of stock
+                                            @elseif ($variation->stock < 10)
+                                                - Low stock ({{ $variation->stock }} left)
+                                            @else
+                                                - In stock
+                                            @endif
+                                        </option>
+                                    @endforeach
+                                </x-select>
                             </form>
 
-                            {{-- Add to Cart Button --}}
-                            <div class="md:px-4">
-                                <form action="{{ route('basket.add', $item->product->id) }}" method="post" class="md:pt-4">
-                                    <x-secondary-button class="whitespace-nowrap">Add to cart</x-secondary-button>
-                                </form>
-                            </div>
-                        </div>
 
+                            {{-- Wishlist & Cart Action Buttons --}}
+                            <div class="flex flex-row items-center md:flex-col md:items-end gap-5">
+                                {{-- Remove from Wishlist Button --}}
+                                <form id="removeFromWishlist"
+                                    action="{{ route('wishlist.remove', $item->product->id) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="px-4" form="removeFromWishlist">
+                                        <img src="{{ asset('icons/utility/heart-default.svg') }}" class="w-6 h-5"
+                                            alt="">
+                                    </button>
+                                </form>
+
+                                {{-- Add to Cart Button --}}
+                                <div class="md:px-4">
+                                    <x-secondary-button form="addToBasket" class="whitespace-nowrap">Add to
+                                        Basket</x-secondary-button>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
                 @endforeach
             </div>
         </div>
