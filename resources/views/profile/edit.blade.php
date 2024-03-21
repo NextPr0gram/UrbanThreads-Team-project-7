ededd3809b17061d376625fb6967f2f8120ce2ad
kishan <kishanjethwa224@gmail.com>
2024-03-21 19:19:05 +0000

    Update modal and profile edit views

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -10,25 +10,25 @@
 
     <div class="py-12">
         <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
-            <div class="p-4 bg-white bg-opacity-40 shadow-md backdrop-blur-sm sm:p-8 border-3 border-navy-blue">
+            <div class="p-4 bg-white bg-opacity-40 shadow-md backdrop-blur-sm sm:p-8 border-2 border-neutral-50 rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 bg-white bg-opacity-40 shadow-md sm:p-8 border-3 border-navy-blue">
+            <div class="p-4 bg-white bg-opacity-40 shadow-md sm:p-8 border-2 border-neutral-50 rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.address-information-form')
                 </div>
             </div>
 
-            <div class="p-4 bg-white bg-opacity-40 shadow-md backdrop-blur-sm sm:p-8 border-3 border-navy-blue">
+            <div class="p-4 bg-white bg-opacity-40 shadow-md backdrop-blur-sm sm:p-8 border-2 border-neutral-50 rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 bg-white bg-opacity-40 shadow-md sm:p-8 border-3 border-navy-blue">
+            <div class="p-4 bg-white bg-opacity-40 shadow-md sm:p-8 border-2 border-neutral-50 rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
