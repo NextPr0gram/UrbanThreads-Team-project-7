4d39de53b52561ead955cc81c7501afdcd95ee97
hanad <220149138@aston.ac.uk>
2024-03-21 23:20:58 +0000

    changes to wishlist page and updated product description

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -22,7 +22,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 1, // Hoodie
                 'name' => 'ChillChic Hoodie',
                 'slug' => 'cool-hoodie',
-                'description' => 'Effortlessly cool with a relaxed silhouette, perfect for casual outings and lounging in style.',
+                'description' => 'Effortlessly cool, this black hoodie features a relaxed cotton silhouette and a design in the middle. Made from 100% cotton, this male-oriented piece is perfect for casual outings and stylish lounging.',
                 'original_price' => '50.00',
                 'selling_price' => '40.00',
                 'image' => '/images/product-images/hoodies/cool-hoodie.png',
@@ -37,7 +37,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 1, // Hoodie
                 'name' => 'CozyComfort Hoodie',
                 'slug' => 'comfy-hoodie',
-                'description' => 'Luxuriously soft fabric provides ultimate comfort, making it the go-to choice for cozy nights and chilly days.',
+                'description' => 'Crafted from 100% cotton, this luxuriously soft pink hoodie offers ultimate comfort, making it the ideal choice for cozy nights and chilly days.',
                 'original_price' => '75.00',
                 'selling_price' => '55.00',
                 'image' => '/images/product-images/hoodies/comfy-hoodie.png',
@@ -52,7 +52,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 1, // Hoodie
                 'name' => 'SportFlex Hoodie',
                 'slug' => 'sporty-hoodie',
-                'description' => 'Engineered for performance, featuring moisture-wicking fabric and ergonomic design, ensuring comfort during intense workouts or outdoor activities.',
+                'description' => 'Engineered for performance, this grey hoodie features a moisture-wicking fabric composed of 70% polyester and 30% cotton, alongside an ergonomic design to ensure comfort during intense workouts or outdoor activities.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/hoodies/sporty-hoodie.png',
@@ -67,7 +67,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 1, // Hoodie
                 'name' => 'UrbanEase Oversize Hoodie',
                 'slug' => 'oversized-hoodie',
-                'description' => 'Make a fashion statement with its oversized silhouette, offering both comfort and urban sophistication for a standout look.',
+                'description' => 'Designed with a unisex appeal, this grey hoodie epitomizes contemporary style with its 100% cotton construction and oversized silhouette. It marries comfort with urban sophistication, offering a distinguished look suitable for a broad spectrum of tastes and preferences.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/hoodies/oversized-hoodie.png',
@@ -82,7 +82,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 1, // Hoodie
                 'name' => 'SvelteFit Hoodie',
                 'slug' => 'slim-fit-hoodie',
-                'description' => 'Tailored for a sleek silhouette, combining style and comfort seamlessly, ideal for those seeking a polished yet relaxed appearance.',
+                'description' => 'Tailored for a sleek silhouette in black, this hoodie combines style and comfort seamlessly with its 70% polyester and 30% cotton blend, ideal for those seeking a polished yet relaxed appearance.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/hoodies/slim-fit-hoodie.png',
@@ -97,7 +97,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 1, // Hoodie
                 'name' => 'WinterWarmth Hoodie',
                 'slug' => 'winter-hoodie',
-                'description' => 'Stay cozy and stylish during colder months with its insulating fabric and trendy design, perfect for layering in the winter season.',
+                'description' => 'Stay cozy and stylish during colder months with this 100% cotton piece, featuring white, fluffy fabric. Its insulating qualities and trendy design make it perfect for layering in the winter season.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/hoodies/winter-hoodie.png',
@@ -159,7 +159,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 2, // T-shirt
                 'name' => 'UrbanEase Oversize Tee',
                 'slug' => 'oversized-t-shirt',
-                'description' => 'Luxuriously oversized, this tee offers laid-back charm with an urban twist.',
+                'description' => 'This luxuriously oversized black tee, crafted from 100% cotton, exudes laid-back charm with an urban twist.',
                 'original_price' => '45.00',
                 'selling_price' => '35.00',
                 'image' => '/images/product-images/t-shirt/oversized-t-shirt.png',
@@ -204,7 +204,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 2, // T-shirt
                 'name' => 'ActiveFlex Performance Tee',
                 'slug' => 'sports-t-shirt',
-                'description' => 'Engineered for peak performance, keeping you cool and focused during any activity.',
+                'description' => 'Engineered for peak performance, this white and grey t-shirt, crafted from a blend of nylon, polyester, and spandex, ensures you stay cool and focused during any activity.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/t-shirt/sports-t-shirt.png',
@@ -236,7 +236,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 3, // Jackets
                 'name' => 'UrbanEdge Leather Jacket',
                 'slug' => 'leather-jacket',
-                'description' => 'Iconic leather, exuding urban sophistication and timeless appeal.',
+                'description' => 'This iconic black jacket, crafted from 100% leather, exudes urban sophistication and timeless appeal.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/jackets/leather-jacket.png',
@@ -266,7 +266,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 3, // Jackets
                 'name' => 'PuffPerfection Jacket',
                 'slug' => 'puffer-jacket',
-                'description' => 'Insulated and cozy, offering warmth and style during cold weather with a modern twist.',
+                'description' => 'This black puffer jacket, crafted from durable nylon or polyester with cozy down or synthetic insulation, offers warmth and style with a modern twist for cold weather.This black puffer jacket, crafted from durable nylon or polyester with cozy down or synthetic insulation, offers warmth and style with a modern twist for cold weather.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/jackets/puffer-jacket.png',
@@ -313,7 +313,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 4, // Trousers
                 'name' => 'ActiveFlex Joggers',
                 'slug' => 'joggers',
-                'description' => 'Designed for comfort and style, perfect for both workouts and casual outings with a sporty edge.',
+                'description' => 'These brown joggers, made from an 80% cotton and 20% polyester blend, are designed for comfort and style, ideal for both workouts and casual outings with a sporty edge.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/trousers/joggers.png',
@@ -343,7 +343,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 4, // Trousers
                 'name' => 'Breezy Shorts',
                 'slug' => 'shorts',
-                'description' => 'Lightweight and comfortable, ideal for warm days and leisurely activities, ensuring freedom of movement.',
+                'description' => 'These grey shorts, crafted from cotton, are lightweight and comfortable, ideal for warm days and leisurely activities, ensuring freedom of movement.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/trousers/shorts.png',
@@ -358,7 +358,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 4, // Trousers
                 'name' => 'CargoReady Cargos',
                 'slug' => 'cargos',
-                'description' => 'Functional and rugged, featuring ample pockets for utility and adventure-ready style.',
+                'description' => 'These green cotton cargos shorts are functional and rugged, featuring ample pockets for utility, making them perfect for adventure-ready style.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/trousers/cargos.png',
@@ -405,7 +405,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 5, // Accessories
                 'name' => 'CozyComfort Beanie',
                 'slug' => 'beanie-hat',
-                'description' => 'Warmth meets style, perfect for cold days with a snug fit and trendy design.',
+                'description' => 'This red beanie, made from a warm blend of cotton and acrylic, offers the perfect combination of snug fit and trendy design, ideal for cold days.',
                 'original_price' => '45.00',
                 'selling_price' => '35.00',
                 'image' => '/images/product-images/accessories/beanie-hat.png',
@@ -420,7 +420,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 5, // Accessories
                 'name' => 'ChillWrap Scarf',
                 'slug' => 'scarf',
-                'description' => 'Versatile and cozy, adding an extra layer of warmth and style to any outfit.',
+                'description' => 'This red scarf, crafted from a blend of wool, linen, silk, and cotton, is versatile and cozy, adding an extra layer of warmth and style to any outfit.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/accessories/scarf.png',
@@ -435,7 +435,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 5, // Accessories
                 'name' => 'StreetSmart Baseball Cap',
                 'slug' => 'baseball-cap',
-                'description' => 'Casual yet cool, offering sun protection with a touch of urban edge.',
+                'description' => 'This blue baseball cap is casual yet cool, offering sun protection with a touch of urban edge, perfect for any outdoor activity.',
                 'original_price' => '45.00',
                 'selling_price' => '30.00',
                 'image' => '/images/product-images/accessories/baseball-cap.png',
@@ -465,7 +465,7 @@ class ProductSeeder extends Seeder
                 'c2_id' => 5, // Accessories
                 'name' => 'SockEssentials',
                 'slug' => 'socks',
-                'description' => 'Comfortable and durable, perfect for everyday wear with a variety of stylish designs.',
+                'description' => 'These white socks, crafted from a blend of polyester, wool, cotton, and spandex, offer comfort and durability, perfect for everyday wear with a variety of stylish designs.',
                 'original_price' => '45.00',
                 'selling_price' => '15.00',
                 'image' => '/images/product-images/accessories/socks.png',
