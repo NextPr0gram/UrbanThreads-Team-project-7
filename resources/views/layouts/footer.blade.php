645d17c1426d4706ac5198e6c39a131a0959cfee
KishanJ22 <kishanjethwa224@gmail.com>
2024-03-22 16:29:40 +0000

    FIx: links in footer either not going to the correct pages or not opening the links in new browser tabs

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,16 @@
 <footer class="sticky top-[100vh]">
+    <div class="flex justify-center py-16">
+        <div class="px-8"><a href="https://www.pinterest.co.uk" target=”_blank”><img class="p-"
+                    src="{{ asset('icons/social-media/pinterest-icon-dark.svg') }}" alt=""></a></div>
+        <div class="px-8"><a href="https://www.facebook.com" target=”_blank”><img class="p-"
+                    src="{{ asset('icons/social-media/facebook-icon-dark.svg') }}" alt=""></a></div>
+        <div class="px-8"><a href="https://www.instagram.com" target=”_blank”><img class="p-"
+                    src="{{ asset('icons/social-media/instagram-icon-dark.svg') }}" alt=""></a></div>
+        <div class="px-8"><a href="https://www.youtube.com" target=”_blank”><img class="p-"
+                    src="{{ asset('icons/social-media/youtube-icon-dark.svg') }}" alt=""></a></div>
+        <div class="px-8"><a href="https://www.tiktok.com" target=”_blank”><img class="p-"
+                    src="{{ asset('icons/social-media/tiktok-icon-dark.svg') }}" alt=""></a></div>
+    </div>
     <div class="bg-neutral-700">
         <div class="mx-auto w-full max-w-screen-xl">
             <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
@@ -46,16 +58,16 @@
                     <h2 class="mb-6 text-lg font-formula1 text-neutral-30">Social channels</h2>
                     <ul class="text-neutral-30">
                         <li class="mb-4">
-                            <a href="https://twitter.com" class="hover:underline">Twitter</a>
+                            <a href="https://twitter.com" class="hover:underline" target=”_blank”>Twitter</a>
                         </li>
                         <li class="mb-4">
-                            <a href="https://www.instagram.com" class="hover:underline">Instagram</a>
+                            <a href="https://www.instagram.com" class="hover:underline" target=”_blank”>Instagram</a>
                         </li>
                         <li class="mb-4">
-                            <a href="https://www.facebook.com" class="hover:underline">facebook</a>
+                            <a href="https://www.facebook.com" class="hover:underline" target=”_blank”>facebook</a>
                         </li>
                         <li class="mb-4">
-                            <a href="https://www.linkedin.com" class="hover:underline">LinkedIn</a>
+                            <a href="https://www.linkedin.com" class="hover:underline" target=”_blank”>LinkedIn</a>
                         </li>
                     </ul>
                 </div>
@@ -63,12 +75,12 @@
                     <h2 class="mb-6 text-lg font-formula1 text-neutral-30">Our Apps</h2>
                     <ul class="text-neutral-30">
                         <li class="mb-4">
-                            <a href="https://play.google.com/store/games?gl=GB&utm_source=emea_Med&utm_medium=hasem&utm_content=May2021&utm_campaign=Evergreen&pcampaignid=MKT-EDR-emea-gb-1707522-Med-hasem-py-Evergreen-May2021-Text_Search_BKWS-test_ctrl_cta_RSA%7CONSEM_kwid_43700008794228187&gad_source=1&gclid=CjwKCAiAivGuBhBEEiwAWiFmYbp1xpnd5Pk1k-hDC6brwy7-hFW3hBfNZ8U67Xb3ZT8C9yOB7kUbXhoCDM0QAvD_BwE&gclsrc=aw.ds"
+                            <a href="https://play.google.com/store/apps?gl=GB" target=”_blank”
                                 class="hover:underline"><img class="h-9"
                                     src="{{ asset('images/playstore-app.svg') }}" alt=""></a>
                         </li>
                         <li class="mb-4">
-                            <a href="https://www.apple.com/uk/store" class="hover:underline"><img class="h-9"
+                            <a href="https://www.apple.com/uk/store" class="hover:underline" target=”_blank”><img class="h-9"
                                     src="{{ asset('images/appstore-app.svg') }}" alt=""></a>
                         </li>
                     </ul>
@@ -81,7 +93,7 @@
                         <x-application-logo :dark="false" class="block mx-2 h-9" />
                     </div>
                     <p class="flex flex-wrap justify-center mb-6 text-base text-neutral-30 sm:mb-0">
-                        © UrbanThreads 2023 | All rights reserved
+                        © UrbanThreads 2024 | All rights reserved
                     </p>
                 </div>
             </div>
