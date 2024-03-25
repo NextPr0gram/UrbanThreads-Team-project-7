3700cb8c305437f5a9378818b25baa3215b94a98
KishanJ22 <kishanjethwa224@gmail.com>
2024-03-25 10:59:29 +0000

    Update forgot password message

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Forgot your password? No problem. Just enter your email address below and we will email you a link that will allow you to choose a new password.') }}
     </div>
 
     <!-- Session Status -->
